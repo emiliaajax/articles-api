956accd26c5e2b0d24b086490eb5fe47b2f49fa7
Emilia Hansson <eh222yn@student.lnu.se>
2023-03-22 14:04:00 +0100

    Configure server.js

---
+++ src/server.js
@@ -0,0 +1,49 @@
+import express from 'express'
+import helmet from 'helmet'
+import logger from 'morgan'
+
+try {
+  const app = express()
+
+  app.use(helmet())
+
+  app.use(logger('dev'))
+
+  app.use(express.json())
+
+  app.use(function (err, req, res, next) {
+    if (!err.status) {
+      const cause = err
+      err = createError(500)
+      err.cause = cause
+    }
+
+    if (req.app.get('env') !== 'development') {
+      return res
+        .status(err.status)
+        .json({
+          status: err.status,
+          message: err.message
+        })
+    }
+
+    // Development only!
+    // Only providing detailed error in development.
+    return res
+      .status(err.status)
+      .json({
+        status: err.status,
+        message: err.message,
+        cause: err.cause ? JSON.stringify(err.cause, Object.getOwnPropertyNames(err.cause)) : undefined,
+        stack: err.stack
+      })
+  })
+
+  app.listen(process.env.PORT, () => {
+    console.log(`Server running at http://localhost:${process.env.PORT}`)
+    console.log('Press Ctrl-C to terminate...')
+  })
+} catch (error) {
+  console.error(error)
+  process.exitCode = 1
+}
