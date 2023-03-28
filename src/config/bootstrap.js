804422c20674567281f06796af810e9acb3510dc
Emilia Hansson <eh222yn@student.lnu.se>
2023-03-28 14:02:35 +0200

    Add webhooks router and update dependencies

--- src/config/bootstrap.js
+++ src/config/bootstrap.js
@@ -1,11 +1,15 @@
 import { PostsController } from '../controllers/posts-controller.js'
 import { UsersController } from '../controllers/users-controller.js'
+import { WebhooksController } from '../controllers/webhooks-controller.js'
 import { PostModel } from '../models/post-model.js'
 import { UserModel } from '../models/user-model.js'
+import { WebhookModel } from '../models/webhook-model.js'
 import { PostRepository } from '../repositories/post-repository.js'
 import { UserRepository } from '../repositories/user-repository.js'
+import { WebhookRepository } from '../repositories/webhook-repository.js'
 import { PostsService } from '../services/posts-service.js'
 import { UsersService } from '../services/users-service.js'
+import { WebhooksService } from '../services/webhook-service.js'
 import { IoCContainer } from '../util/IoCContainer.js'
 
 const iocContainer = new IoCContainer()
@@ -14,6 +18,7 @@ iocContainer.register('ConnectionString', process.env.DB_CONNECTION_STRING)
 
 iocContainer.register('PostModelType', PostModel, { type: true })
 iocContainer.register('UserModelType', UserModel, { type: true })
+iocContainer.register('WebhookModelType', WebhookModel, { type: true })
 
 iocContainer.register('PostRepositorySingleton', PostRepository, {
   dependencies: [
@@ -25,6 +30,13 @@ iocContainer.register('PostRepositorySingleton', PostRepository, {
 iocContainer.register('UserRepositorySingleton', UserRepository, {
   dependencies: [
     'UserModelType'
+  ],
+  singleton: true
+})
+
+iocContainer.register('WebhookRepositorySingleton', WebhookRepository, {
+  dependencies: [
+    'WebhookModelType'
   ],
   singleton: true
 })
@@ -43,6 +55,13 @@ iocContainer.register('UsersServiceSingleton', UsersService, {
   singleton: true
 })
 
+iocContainer.register('WebhooksServiceSingleton', WebhooksService, {
+  dependencies: [
+    'WebhookRepositorySingleton'
+  ],
+  singleton: true
+})
+
 iocContainer.register('PostsController', PostsController, {
   dependencies: [
     'PostsServiceSingleton'
@@ -52,6 +71,12 @@ iocContainer.register('PostsController', PostsController, {
 iocContainer.register('UsersController', UsersController, {
   dependencies: [
     'UsersServiceSingleton'
+  ]
+})
+
+iocContainer.register('WebhooksController', WebhooksController, {
+  dependencies: [
+    'WebhooksServiceSingleton'
   ]
 })
 
---
+++ src/routes/api/v1/webhooks-router.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+export const router = express.Router()
+
+/**
+ * Resolves a WebhooksController object from the IoC container.
+ *
+ * @param {object} req - Express request object.
+ * @returns {object} An object that can act as a WebhooksController object.
+ */
+const resolveWebhooksController = (req) => req.app.get('container').resolve('WebhooksController')
+
+router.post('/register', (req, res, next) => resolveWebhooksController(req).register(req, res, next))
